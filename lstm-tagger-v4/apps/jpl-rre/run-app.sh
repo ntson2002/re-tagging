366f619264e44214025b40e93f51caee3d707af4
Son Nguyen <ntson2002@gmail.com>
2017-12-13 16:50:24 +0900

    remove unused

--- lstm-tagger-v4/apps/jpl-rre/run-app.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-MODEL_PATH=$PWD/rre-sample-scripts/models/jpl-rre-f36
-
-#cd $HOME/Bitbucket/lstm-crf-tagging/lstm-tagger-v4/
-cd ../../../lstm-tagger-v4
-export PYTHONPATH=$PWD:$PYTHONPATH
-python ./apps/jpl-rre/rre-api.py --save $MODEL_PATH --port 8126
